d02316374078c68265761c9111b6741d3de4cce2
Soph Robert-Hayek <sophie.robert@univ-lorraine.fr>
2024-01-17 11:07:25 +0100

    ✨ Add morphological analysis

---
+++ src/backend/contexts/morphological_analysis/router.py
@@ -0,0 +1,39 @@
+"""Endpoints to retrieve the lexicometirc analysis of words found within the QWB database.
+"""
+import typing as t
+import json
+from fastapi import APIRouter, Request, Depends, Response
+
+
+def sql_database(request: Request):
+    """Access the mongo database from a Starlette/FastAPI request"""
+    return request.app.state.database
+
+
+router = APIRouter(
+    prefix="/lexicometric",
+    tags=["Lexicometric Analysis"]
+)
+
+
+@router.get("/{word}")
+async def get_word_analysis(word: str,
+                            manuscript: t.Optional[str] = None,
+                            column: t.Optional[str] = None,
+                            line: t.Optional[str] = None,
+                            database=Depends(sql_database)):
+    """Given a word, list all lexicometric analysis available for this word.
+    Optionally, can be filtered by manuscript, column and line.
+    If manuscript is not filled out, column and line are ignored.
+    If column is not filled out, line is ignored.
+    """
+    word_analysis = await database.get_word_morphological_analysis(word=word,
+                                                                   manuscript=manuscript,
+                                                                   column=column,
+                                                                   line=line)
+    if not word_analysis:
+        return Response(status_code=404, content=f"No morphological analysis for word {word} at "
+                        "the provided location.")
+    return Response(content=json.dumps({word: word_analysis},
+                    ensure_ascii=False).encode('utf8'),
+                    media_type="application/json")
