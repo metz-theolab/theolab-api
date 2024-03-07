6f08d85ab1552e3e317dc614acc967c75c5dfdf8
Guillaume Charbonnier <guillaume.charbonnier@araymond.com>
2024-03-07 15:32:20 +0100

    ✨ Add options to command line interface

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,6 +1,8 @@
 from functools import partial
+from pathlib import Path
 import uvicorn
 import click
+import yaml
 
 from backend.api.app import create_app
 from backend.settings.settings import APISettings
@@ -11,9 +13,37 @@ from backend.settings.settings import APISettings
               default=False,
               is_flag=True,
               help='Only activate endpoints related to scribes')
-def main(scribes_only: bool):
-    # TODO: parse settings from YAML files
-    api_settings = APISettings()
+@click.option('--port',
+              type=int,
+              metavar="NUMBER",
+              default=None,
+              help='Port to run the API on')
+@click.option('--host',
+              type=str,
+              metavar="ADDRESS",
+              default=None,
+              help='Host to run the API on')
+@click.option('--config',
+              type=str,
+              metavar="PATH",
+                default=None,
+                help='Path to config file')
+def main(
+    scribes_only: bool,
+    port: int,
+    host: str,
+    config: str,
+):
+    raw_settings = dict()
+    if config is not None:
+        config_filepath = Path(config).expanduser().resolve(True)
+        config_content = yaml.safe_load(config_filepath.read_text())
+        raw_settings.update(config_content)
+    if port is not None:
+        raw_settings["port"] = port
+    if host is not None:
+        raw_settings["host"] = host
+    api_settings = APISettings(**raw_settings)
     uvicorn.run(
         port=api_settings.port,
         host=api_settings.host,
